c4b139ad26e2f322c0c733c46062aa12f2713191
Alfonso Zendejas <omarzenlab@gmail.com>
2023-08-31 14:47:02 -0600

    Structure, views and styles created

---
+++ views/content.view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <script src="https://use.fontawesome.com/b19e15daf2.js"></script>
+    
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Content</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Site Content</h1>
+        <a href="close.php">Close session</a>
+
+        <hr class="border">
+
+        <div class="content">
+            <article>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique distinctio, beatae consequuntur exercitationem sed, nihil aliquam magni quam nam quasi doloremque, assumenda dicta iusto consequatur harum atque? Eos laudantium veniam quaerat modi possimus commodi corrupti aliquam voluptatibus quis! Dicta, ratione?
+                </p>
+
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla vel odit repellendus unde veritatis! Omnis expedita hic quod illo dignissimos, eaque, quo atque asperiores pariatur, sequi cupiditate laudantium eos excepturi.
+                </p>
+            </article>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ views/register.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <script src="https://use.fontawesome.com/b19e15daf2.js"></script>
+    
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Site Register</h1>
+
+        <hr class="border">
+    </div>
+
+
+</body>
+</html>
