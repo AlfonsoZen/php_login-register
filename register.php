40ee642ab4010fa9e7c2e253a2eae92fec53df16
Alfonso Zendejas <omarzenlab@gmail.com>
2023-09-18 11:00:21 -0600

    Validate the Password and Encrypt it

--- register.php
+++ register.php
@@ -1,23 +1,25 @@
 <?php session_start();
 
-if( isset($_SESSION['user']) ) {
-    header( 'Location: index.php');
+if (isset($_SESSION['user'])) {
+    header('Location: index.php');
 }
 
-if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
-    $user = filter_var( strtolower( $_POST['user'] ), FILTER_SANITIZE_STRING );
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = filter_var(strtolower($_POST['user']), FILTER_SANITIZE_STRING);
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
 
-    // echo "$user, $password, $password2";
-
     $errors = '';
 
-    if( empty($user) || empty($password) || empty($password2) ) {
+    if (empty($user) || empty($password) || empty($password2)) {
         $errors .= "<li>All fields must be filled</li>";
     } else {
         try {
-			$conn = new PDO('mysql:host=localhost;dbname=php_login', 'root', '');
+            $conn = new PDO('mysql:host=localhost;dbname=php_login', 'root', '');
+
+            if (!$conn) {
+                throw new PDOException('Connection failed');
+            }
 
             $statement = $conn->prepare('SELECT * FROM users WHERE name = :user LIMIT 1');
             $statement->bindParam(':user', $user);
@@ -26,15 +28,33 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
             $result = $statement->fetch();
 
             if ($result != false) {
-                	$errors .= '<li>El nombre de usuario ya existe</li>';
-            } 
-		} catch (PDOException $e) {
-			echo "Error:" . $e->getMessage();
-		}
-    }   
+                $errors .= '<li>The username already exists</li>';
+            }
+
+            $password = hash('sha512', $password);
+            $password2 = hash('sha512', $password2);
+
+            if ($password != $password2) {
+                $errors .= '<li>Passwords are not the same.</li>';
+            }
+        } catch (PDOException $e) {
+            echo "Error:" . $e->getMessage();
+        }
+    }
+
+    if ($errors == '') {
+        $statement = $conn->prepare('INSERT INTO users(id, name, password) VALUES (null, :name, :password)');
+        $statement->bindParam(':name', $user);
+        $statement->bindParam(':password', $password);
+        $statement->execute();
+    }
+
+    header('Location: login.php');
+
 }
 
 require 'views/register.view.php';
+
 session_unset();
 session_destroy();
 ?>
