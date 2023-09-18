5ba1932d34e4a4b91d7c41d922ce9df0dd1af653
Alfonso Zendejas <omarzenlab@gmail.com>
2023-09-18 12:03:07 -0600

    Content and Signing Out Protected

--- register.php
+++ register.php
@@ -12,33 +12,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = '';
 
     if (empty($user) || empty($password) || empty($password2)) {
-        $errors .= "<li>All fields must be filled</li>";
+        $errors = "<li>All fields must be filled</li>";
+
     } else {
         try {
             $conn = new PDO('mysql:host=localhost;dbname=php_login', 'root', '');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-            if (!$conn) {
-                throw new PDOException('Connection failed');
-            }
-
-            $statement = $conn->prepare('SELECT * FROM users WHERE name = :user LIMIT 1');
-            $statement->bindParam(':user', $user);
-            $statement->execute();
-
-            $result = $statement->fetch();
-
-            if ($result != false) {
-                $errors .= '<li>The username already exists</li>';
-            }
-
-            $password = hash('sha512', $password);
-            $password2 = hash('sha512', $password2);
-
-            if ($password != $password2) {
-                $errors .= '<li>Passwords are not the same.</li>';
-            }
         } catch (PDOException $e) {
             echo "Error:" . $e->getMessage();
+        }
+
+        $statement = $conn->prepare('SELECT * FROM users WHERE name = :user LIMIT 1');
+        $statement->bindParam(':user', $user);
+        $statement->execute();
+
+        $result = $statement->fetch();
+
+        if ($result != false) {
+            $errors .= '<li>The username already exists</li>';
+        }
+
+        $password = hash('sha512', $password);
+        $password2 = hash('sha512', $password2);
+
+        if ($password != $password2) {
+            $errors .= '<li>Passwords are not the same.</li>';
         }
     }
 
@@ -47,9 +46,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $statement->bindParam(':name', $user);
         $statement->bindParam(':password', $password);
         $statement->execute();
+
+        header('Location: login.php');
     }
 
-    header('Location: login.php');
 
 }
 
